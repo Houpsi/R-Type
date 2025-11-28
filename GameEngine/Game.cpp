22f17f21f55a7f4a5e596a7a1d9c31adcedd11ff
Aleksandra <aleksandra.racine@epitech.eu>
2025-11-28 21:04:34 +0100

    feat(ecs): add enemy and clock

--- GameEngine/Game.cpp
+++ GameEngine/Game.cpp
@@ -1,11 +1,17 @@
-//
-// Created by aleks on 27/11/2025.
-//
+/*
+** EPITECH PROJECT, 2025
+** R_Type
+** File description:
+** Game
+*/
+
 
 #include "Game.hpp"
 
+#include "Components/Enemy.hpp"
 #include "Components/Health.hpp"
 #include "Components/Position.hpp"
+#include "Components/Sprite.hpp"
 #include "Systems/InputSystem.hpp"
 #include "Systems/MovementSystem.hpp"
 #include "Systems/RenderSystem.hpp"
@@ -20,26 +26,35 @@ Game::~Game() {
 
 void Game::run() {
     auto player = _ecs.createEntity();
-    sf::RenderWindow window(sf::VideoMode(static_cast<int>(100), static_cast<int>(100)), "SFML window");
+    auto enemy = _ecs.createEntity();
+    sf::RenderWindow window(sf::VideoMode(static_cast<int>(1000), static_cast<int>(800)), "SFML window");
+    sf::Clock clock;
 
 
     player->addComponent<Health>(100);
     player->addComponent<Position>(1, 2);
-    player->addComponent<InputPlayer>(1);
+    player->addComponent<InputPlayer>();
+    player->addComponent<Sprite>("image.png");
+
+    enemy->addComponent<Health>(100);
+    enemy->addComponent<Position>(100, 100);
+    enemy->addComponent<Enemy>();
+    enemy->addComponent<Sprite>("image.png");
 
 
-    _ecs.addSystem<InputSystem>(false, false, false, false);
-    _ecs.addSystem<MovementSystem>(1);
+    _ecs.addSystem<InputSystem>();
+    _ecs.addSystem<MovementSystem>();
     _ecs.addSystem<RenderSystem>(window);
 
     while (window.isOpen()) {
+        float dt = clock.restart().asSeconds();
         sf::Event event{};
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        _ecs.setDeltaTime(dt);
         _ecs.updateSystems();
-
     }
 }
