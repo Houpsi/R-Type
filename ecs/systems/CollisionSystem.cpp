fb7a62fa8815cbe37b41cae472b8b7f85e97b602
HeleneH <helene.houplain@epitech.eu>
2025-12-02 17:31:55 +0100

    feat(ecs): implement `CollisionSystem` update method with basic structure and add necessary includes

--- ecs/systems/CollisionSystem.cpp
+++ ecs/systems/CollisionSystem.cpp
@@ -1,9 +1,21 @@
-//
-// Created by aleks on 27/11/2025.
-//
+/*
+** EPITECH PROJECT, 2025
+** r-type_client
+** File description:
+** CollisionSystem
+*/
 
 #include "CollisionSystem.hpp"
 
 namespace ECS {
+void CollisionSystem::update(ECS::EcsManager &ecs)
+{
+    for (auto& entity: ecs.getEntities()) {
+        auto collision = entity->getComponent<Collision>();
+        if (!collision) continue;
+//        auto tmp = entity->getComponent<Position>()
+
+    }
+}
 
 }
--- ecs/systems/CollisionSystem.hpp
+++ ecs/systems/CollisionSystem.hpp
@@ -1,13 +1,21 @@
-//
-// Created by aleks on 27/11/2025.
-//
+/*
+** EPITECH PROJECT, 2025
+** r-type_client
+** File description:
+** CollisionSystem
+*/
 
 #ifndef BOOTSTRAP_COLLISIONSYSTEM_HPP
 #define BOOTSTRAP_COLLISIONSYSTEM_HPP
-
+#include "Sprite.hpp"
+#include "EcsManager.hpp"
+#include "Collision.hpp"
 
 namespace ECS {
-class CollisionSystem {
+class CollisionSystem: public System {
+  public:
+    CollisionSystem() = default;
+    void update(EcsManager &ecs) override;
 };
 }
 
