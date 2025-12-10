2159bb12b8fa60dfff80fc84f8c027916286e84b
Aleksandra <aleksandra.racine@epitech.eu>
2025-12-10 16:20:48 +0100

    feat(ecs): add class game

---
+++ server/src/server/Game.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2025
+** r-type_client
+** File description:
+** Game
+*/
+
+#ifndef BOOTSTRAP_GAME_HPP
+    #define BOOTSTRAP_GAME_HPP
+#include <random>
+#include "EcsManager.hpp"
+#include "components/Enemy.hpp"
+#include "components/Health.hpp"
+#include "components/Position.hpp"
+#include "components/Sprite.hpp"
+#include "components/Sound.hpp"
+#include "systems/InputSystem.hpp"
+#include "systems/MovementSystem.hpp"
+#include "systems/RenderSystem.hpp"
+#include "systems/VelocitySystem.hpp"
+#include "systems/ShootSystem.hpp"
+#include "systems/CollisionSystem.hpp"
+#include "components/Collision.hpp"
+
+#define WINDOW_X 1920
+#define WINDOW_Y 1080
+
+class Game {
+public:
+    Game() : _ecs(ecs::EcsManager()) {}
+    ~Game() = default;
+    void run();
+private:
+    ecs::EcsManager _ecs;
+};
+
+#endif //BOOTSTRAP_GAME_HPP
