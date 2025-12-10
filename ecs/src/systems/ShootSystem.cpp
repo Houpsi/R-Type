6f93df7e71a0b97af50a261c5b5c9602f99ebb0c
HeleneH <helene.houplain@epitech.eu>
2025-12-10 16:27:59 +0100

    fix(shoot): fix shoot and animated bullet

--- ecs/src/systems/ShootSystem.cpp
+++ ecs/src/systems/ShootSystem.cpp
@@ -29,10 +29,11 @@ namespace ecs {
                         shoot->setTimeSinceLastShot(0);
                         projectile->addComponent<ecs::Position>(
                             entity->getComponent<Position>()->getX(), entity->getComponent<Position>()->getY());
-                        projectile->addComponent<Velocity>(400, 0.5);
+                        projectile->addComponent<Velocity>(400, 1);
                         projectile->addComponent<Shoot>(
                             entity->getComponent<Shoot>()->getDamage(), entity->getComponent<Shoot>()->getCooldown());
                         projectile->addComponent<Sprite>("./assets/r-typesheet30a.gif");
+                        projectile->addComponent<Animation>(std::pair<int, int>(34, 34), 0, 3);
                         projectile->addComponent<Collision>(ecs::TypeCollision::PLAYER_PROJECTILE, 10, 10);
                     }
                 }
