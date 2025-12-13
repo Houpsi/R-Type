a13096a2a0e11538534d136ada283be6785d21cd
raphaelGONCALVESDOSSANTOS <raphael.goncalves-dos-santos@epitech.eu>
2025-12-13 16:09:23 +0100

    fix(packet_data): move code in cpp files

--- common/src/packet_data/InputPacket.hpp
+++ common/src/packet_data/InputPacket.hpp
@@ -18,15 +18,8 @@ namespace cmn {
         uint8_t keyState;
     };
 
-    sf::Packet &operator << (sf::Packet &packet, const inputPacket &packetStruct)
-    {
-        return packet << packetStruct.key << packetStruct.keyState;
-    }
-
-    sf::Packet &operator >> (sf::Packet &packet, inputPacket &packetStruct)
-    {
-        return packet >> packetStruct.key >> packetStruct.keyState;
-    }
+    sf::Packet &operator << (sf::Packet &packet, const inputPacket &packetStruct);
+    sf::Packet &operator >> (sf::Packet &packet, inputPacket &packetStruct);
 
 }
 
