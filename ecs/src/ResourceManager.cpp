69a5779516b92674cc87192a5ad76449bcf7b378
AxelDesert <axel.desert@epitech.eu>
2025-12-13 16:09:18 +0100

    feat(resource-manager): added ResourceManager to the cmakelist

---
+++ ecs/src/ResourceManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by axel on 12/10/25.
+//
+
+#include "ResourceManager.hpp"
---
+++ ecs/src/ResourceManager.hpp
@@ -0,0 +1,14 @@
+//
+// Created by axel on 12/10/25.
+//
+
+#ifndef R_TYPE_RESOURCEMANAGER_HPP
+#define R_TYPE_RESOURCEMANAGER_HPP
+
+
+class ResourceManager
+{
+};
+
+
+#endif// R_TYPE_RESOURCEMANAGER_HPP
