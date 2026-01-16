615fb78a705aef41f74bf1c745a7e9a0d4c9da5a
HeleneH <helene.houplain@epitech.eu>
2026-01-16 18:24:18 +0100

    feat(render_system): add score and text rendering logic

--- ecs/src/systems/RenderSystem.cpp
+++ ecs/src/systems/RenderSystem.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "RenderSystem.hpp"
+#include "components/Score.hpp"
 #define SIZE_X_PLAYER 33
 #define SIZE_Y_PLAYER 17
 
@@ -13,10 +14,12 @@ namespace ecs {
 void RenderSystem::update(EcsManager &ecs)
 {
     _window.clear(sf::Color::Black);
-    const auto entities = ecs.getEntitiesWithComponent<Sprite>();
+    const auto entities = ecs.getEntities();
 
     for (const auto& entity : entities) {
         auto spriteComp = entity->getComponent<Sprite>();
+        auto textComp = entity->getComponent<Text>();
+        auto scoreComp = entity->getComponent<Score>();
         auto positionComp = entity->getComponent<Position>();
         if (spriteComp) {
             auto sprite = spriteComp->getSprite();
@@ -24,6 +27,15 @@ void RenderSystem::update(EcsManager &ecs)
                 sprite.setPosition({positionComp->getX(), positionComp->getY()});
             }
             _window.draw(sprite);
+        }
+        if (textComp) {
+            auto& t = textComp->getText();
+            if (positionComp)
+                t.setPosition({positionComp->getX(), positionComp->getY()});
+            if (scoreComp) {
+                t.setString("Score: " + std::to_string(scoreComp->getScore()));
+            }
+            _window.draw(t);
         }
     }
     _window.display();
